02dc01796fef0b2c15c30b18d6389f16485aa727
ADMIN <ADMIN@NGUYENNTTD01427>
2025-12-27 10:24:46 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+import pic1 from "./src/assets/pic1.png";
+import pic2 from "./src/assets/pic2.png";
+import pic3 from "./src/assets/pic3.png";
+import pic4 from "./src/assets/pic4.png";
+
+export const data = [
+  {
+    image: pic1,
+    title: "Components",
+    desc: "Khối xây dựng giao diện cơ bản - kết hợp nhiều thành phần để tạo nên ứng dụng.",
+  },
+  {
+    image: pic2,
+    title: "JSX",
+    desc: "Kết hợp HTML và JavaScript để tạo giao diện động và mạnh mẽ.",
+  },
+  {
+    image: pic3,
+    title: "Props",
+    desc: "Truyền dữ liệu vào thành phần để làm nó linh hoạt và tái sử dụng.",
+  },
+  {
+    image: pic4,
+    title: "State",
+    desc: "Dữ liệu được React quản lý, khi thay đổi sẽ tự động làm mới giao diện.",
+  },
+];
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import logo from "./assets/logo-tuhoc.png";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <img src={logo} alt="Tự Học" />
+        <h1>React Tuhoc.cc</h1>
+        <p>
+          Học React - Khám phá cách xây dựng ứng dụng linh hoạt, hiện đại, và
+          đầy sáng tạo!
+        </p>
+      </header>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Khái niệm chính trong React</h2>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
