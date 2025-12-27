b3f3ea19a27b1d58b20ab25e69b42dfd653932c5
ADMIN <ADMIN@NGUYENNTTD01427>
2025-12-27 11:47:01 +0700

    14.6 onclick

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,10 @@
 export default function TabButton({children}) {
+  function handleClick() {
+    alert("Nút bấm được click");
+  }
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
