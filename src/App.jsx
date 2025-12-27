60e53dcc5cdc1daf9b06b9e4ea1fc4c251ed0e25
ADMIN <ADMIN@NGUYENNTTD01427>
2025-12-27 11:39:48 +0700

    props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {myData} from "../data.js";
 import Header from "./components/Header/Header.jsx";
 import MainContent from "./components/MainContent/MainContent.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -15,6 +16,16 @@ function App() {
             <MainContent {...myData[2]} />
             <MainContent {...myData[3]} />
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </>
